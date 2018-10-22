3ea4c69425a6778fa55dbce746a288f363309488
hpLiu <374708234@qq.com>
2018-10-22 12:59:24 +0800

    audio

--- ppgame/frameworks/runtime-src/Classes/AppDelegate.cpp
+++ ppgame/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -4,7 +4,7 @@
 #include "scripting/lua-bindings/manual/lua_module_register.h"
 
 // #define USE_AUDIO_ENGINE 1
-// #define USE_SIMPLE_AUDIO_ENGINE 1
+ #define USE_SIMPLE_AUDIO_ENGINE 1
 
 #if USE_AUDIO_ENGINE && USE_SIMPLE_AUDIO_ENGINE
 #error "Don't use AudioEngine and SimpleAudioEngine at the same time. Please just select one in your game!"
